9b9123951a9bd1e1b9fd22b8ba85255dda1f7e75
mengxiang.zhou <mengxiang.zhou@weimob.com>
2021-02-02 14:47:17 +0800

    cell组件开发完成

--- components/cell/index.tsx
+++ components/cell/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Icon, Ripple } from '@/components/index';
 import { classnames } from '../_utils/index';
 import './index.less';
 
 export interface CellProps {
   title: React.ReactNode;
   label: React.ReactNode;
-  arrow: 'left' | 'right' | 'top' | 'none';
+  arrow: 'left' | 'right' | 'up' | 'down' | 'none'; // 箭头方向
   className: string;
-  center: boolean;
-  clickable: boolean;
+  center: boolean; // 上下居中
+  clickable: boolean; // 是否开启点击反馈 点击高亮
+  children: React.ReactNode;
+  ripple: boolean;
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  style: React.CSSProperties;
 }
 const Cell: React.FC<Partial<CellProps>> = props => {
-  const { title, label, center, className, clickable, arrow } = props;
-  console.log('arrow', arrow);
+  const { title, label, center, style, className, clickable, arrow, children, ripple } = props;
+
+  function onClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    props.onClick(event);
+  }
   return (
     <div
       className={classnames({
@@ -21,17 +29,26 @@ const Cell: React.FC<Partial<CellProps>> = props => {
         'wei-cell-center': center,
         'wei-cell-clickable': clickable
       })}
+      style={style}
+      onClick={onClick}
     >
       <div className={'wei-cell-title'}>
         <div>{title}</div>
         {label !== undefined && <div className={'wei-cell-label'}>{label}</div>}
       </div>
+      <div className='wei-cell-value wei-ellipsis'>{children}</div>
+      {arrow !== 'none' && <Icon className='arrow-icon' type={`arrow-${arrow}`} />}
+      {ripple && <Ripple className='wei-cell-ripple' />}
     </div>
   );
 };
 
 Cell.defaultProps = {
-  arrow: 'none'
+  arrow: 'none',
+  center: false,
+  ripple: false,
+  clickable: false,
+  onClick: () => undefined
 };
 
 export default Cell;
--- site/page/Cell/index.tsx
+++ site/page/Cell/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { Cell } from '@/components/index';
+import './index.less';
 
 function CellDemo() {
   return (
     <div>
-      <Cell title='标题' label='内容描述' />
+      <div className='demo-block__title'>基础用法</div>
+      <Cell title='标题' label='hellow这里是个人中心' center clickable>
+        居中内容
+      </Cell>
+      <Cell title='标题'>
+        <div>个人中心</div>
+      </Cell>
+      <div className='demo-block__title'>展示箭头</div>
+      <Cell title='标题' arrow='right' clickable>
+        个人中心
+      </Cell>
+      <Cell title='标题' arrow='down' clickable>
+        个人中心
+      </Cell>
+      <div className='demo-block__title'>水波纹</div>
+      <Cell title='标题' arrow='right' ripple></Cell>
+      <Cell title='标题' arrow='right' ripple>
+        水波纹好看
+      </Cell>
     </div>
   );
 }
